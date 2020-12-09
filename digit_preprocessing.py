803e82de9575657475bb1fb687513ef69a5a4567
EDENLAYN-D-MINACCI <minatchi78@outlook.fr>
2020-12-09 06:44:18 +0100

    Replace Tensorflow by Keras

--- digit_preprocessing.py
+++ digit_preprocessing.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
+from keras.models import load_model
 import cv2, base64
 import numpy as np
 
 
 #loading the model
-model = tf.keras.models.load_model("C:/Users/AKUMA/Desktop/Repository/digit-recognition/mnist_digit_recog_cnn_2-conv-128-nodes-1-dense-17-04-2020_00-09_Ver16.h5")
+model = load_model("C:/Users/AKUMA/Desktop/Repository/digit-recognition/mnist_digit_recog_cnn_2-conv-128-nodes-1-dense-17-04-2020_00-09_Ver16.h5")
 
 
 def base64_to_nparray(URL):
